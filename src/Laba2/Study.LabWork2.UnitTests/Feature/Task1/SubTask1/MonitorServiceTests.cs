c2291d22ecb25177fa8a1286bc338ec8e9d1991f
Varya Masina <masinavarvarad@gmail.com>
2026-05-25 02:30:12 +0300

    финал

--- src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MonitorServiceTests.cs
+++ src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MonitorServiceTests.cs
@@ -1,6 +1,20 @@
+using NUnit.Framework;
+
 namespace Study.LabWork2.UnitTests.Feature.Task1.SubTask1;
 
 [TestFixture]
 public sealed class MonitorServiceTests
 {
+    [Test]
+    public void CountPrimes_Range1To10000_ReturnsCorrectCount()
+    {
+        // создаем сервис через точный путь в проекте
+        var service = new study.labwork2.feature.task1.subtask1.MonitorService();
+
+        // вызываем метод подсчета
+        var result = service.CountPrimes(1, 10000, 4);
+
+        // проверяем результат
+        Assert.That(result.PrimeCount, Is.EqualTo(1229));
+    }
 }
--- src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/SemaphoreService.cs
+++ src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/SemaphoreService.cs
@@ -1,14 +1,74 @@
+using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1.DtoModels;
 
-namespace Study.LabWork2.Feature.Task1.SubTask1;
+namespace study.labwork2.feature.task1.subtask1;
 
-/// <summary>
-/// Версия 3. Использует Semaphore для синхронизации
-/// </summary>
 public sealed class SemaphoreService : IPrimeCounter
 {
-    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount) => throw new NotImplementedException();
+    private static readonly Semaphore sem = new Semaphore(1, 1);
 
-    public string GetVersionName() => throw new NotImplementedException();
+    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount)
+    {
+        int totalPrimes = 0;
+        List<int> foundList = new List<int>();
+        Thread[] threads = new Thread[threadCount];
+        int range = (end - start + 1) / threadCount;
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            int tStart = start + i * range;
+            int tEnd = (i == threadCount - 1) ? end : tStart + range - 1;
+
+            threads[i] = new Thread(() =>
+            {
+                for (int num = tStart; num <= tEnd; num++)
+                {
+                    if (IsPrime(num))
+                    {
+                        sem.WaitOne();
+                        try
+                        {
+                            totalPrimes++;
+                            foundList.Add(num);
+                        }
+                        finally
+                        {
+                            sem.Release();
+                        }
+                    }
+                }
+            });
+            threads[i].Start();
+        }
+
+        foreach (var t in threads) t.Join();
+        sw.Stop();
+
+        return new PrimeCountResultDto
+        {
+            PrimeCount = totalPrimes,
+            ExecutionTime = sw.Elapsed,
+            ThreadCount = threadCount,
+            SynchronizationType = GetVersionName(),
+            FoundPrimes = foundList
+        };
+    }
+
+    public string GetVersionName() => "Semaphore";
+
+    private static bool IsPrime(int num)
+    {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
 }
